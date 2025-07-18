834305c1c37830ea7bf15d454a931063136f4f2e
Orach Paul Francis <157469714+ORACHPAULFRANCIS@users.noreply.github.com>
2025-07-18 03:28:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Business-Website/", // 👈 Crucial for GitHub Pages
+//  base: "/Business-Website/", // Crucial for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
